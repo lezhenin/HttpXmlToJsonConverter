e8908394f942e6486e963861ca8f0e2a11098eb6
Yuriy Lezhenin <yuriy.lezhenin@spbpu.com>
2018-12-14 17:44:11 +0300

    Refactoring

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+const xmlParser = require('xml2json');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-
+/** Instance of http express server. */
 const app = express();
 
+/** Set logger for express server. */
 app.use(logger('dev'));
+
+/** Add middleware function for text body parsing. */
 app.use(bodyParser.text({type: 'text/xml'}));
 
-app.use('/', indexRouter);
+/** Set handler for post request. */
+app.post('/', handler);
+
+/** @function
+ *  @name hanlder
+ *
+ *  Post handler for http server. Interpret body as
+ *  string and try to convert xml to json. On success
+ *  send json string with code 200.
+ *
+ *  @param req - request object
+ *  @param res - response object
+ *  @param next - next express middleware function
+ */
+function handler(req, res, next) {
+    try {
+        const jsonString = xmlParser.toJson(req.body);
+        res.type('text/json');
+        res.status(200).send(jsonString);
+    } catch (e) {
+        res.status(400).send(e.message)
+    }
+}
+
 
 module.exports = app;
